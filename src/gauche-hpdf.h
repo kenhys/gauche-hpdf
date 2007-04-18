216bd6431054b49cd92c6c8ead4388d75b175314
HAYASHI Kentaro <kenhys@gmail.com>
2007-04-18 05:54:16 +0000

    add --enable-debug macros.

--- src/gauche-hpdf.h
+++ src/gauche-hpdf.h
@@ -18,6 +18,18 @@ SCM_DECL_BEGIN
 
 extern ScmObj test_hpdf(void);
 
+#ifdef GAUCHE_HPDF_DEBUG
+#define GAUCHE_HPDF_FUNC(func) Scm_Printf(SCM_CURERR, "#=> %s()\n", func)
+#define GAUCHE_HPDF_STR(key, value) Scm_Printf(SCM_CURERR, "%s => %s\n", (key), (value))
+#define GAUCHE_HPDF_INT(key, value) Scm_Printf(SCM_CURERR, "%s => %d\n", (key), (value))
+#define GAUCHE_HPDF_HEX(key, value) Scm_Printf(SCM_CURERR, "%s => %x\n", (key), (value))
+#else
+#define GAUCHE_HPDF_FUNC(func)
+#define GAUCHE_HPDF_STR(key, value)
+#define GAUCHE_HPDF_INT(key, value)
+#define GAUCHE_HPDF_HEX(key, value)
+#endif
+
 /* Epilogue */
 SCM_DECL_END
 
