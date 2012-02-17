46b74498cb163fdc84ea47874ccf6e851e7d469e
HAYASHI Kentaro <kenhys@gmail.com>
2012-02-17 20:56:18 +0900

    add test-subsection

--- lib/hpdf.scm
+++ lib/hpdf.scm
@@ -14,6 +14,13 @@
 ;; Put your Scheme definitions here
 ;;
 
+(define (test-subsection msg)
+  ;;(format #t "~79,,,'.,,@a\n" (format #f "[~a]" msg)))
+  (format #t "~79,,,' ,,@a\n" (format #f "[~a]" msg)))
+(define (test-subsubsection msg)
+  (format #t "#=> ~75,,,'.,,a \n" (format #f "~a " msg)))
+
+
 ;; Epilogue
 (provide "hpdf")
 
