4c29358769525da2dc0186e5bba893bc54577f49
HAYASHI Kentaro <kenhys@gmail.com>
2012-02-22 01:56:59 +0900

    fix height.

--- test/encryption.scm
+++ test/encryption.scm
@@ -9,8 +9,8 @@
 
 (define (main args)
   (let* ([pdf (hpdf-new)]
-	 [page (hpdf-add-page pdf)]
-	 [font (hpdf-get-font pdf "Helvetica" "")]
+	 [page (add-page pdf)]
+	 [font (font pdf "Helvetica" "")]
 	 [tw 0]
 	 [filename (if (rxmatch #/.*test\/.*\.scm$/ *program-name*)
 		       "test/encryption.pdf" "encryption.pdf")])
@@ -20,7 +20,7 @@
     (font-and-size! page font 20)
     (set! tw (text-width page text))
     (move-text-pos page (/ (- (width page) tw) 2)
-			     (/ (- (height  page) 20 2)))
+			     (/ (- (height  page) 20) 2))
 
     (show-text page text)
     (end-text page)
