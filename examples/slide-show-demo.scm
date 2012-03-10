3f2c61983fa7bdf4e0a0ce0f1b10fbab8c644cf4
HAYASHI Kentaro <kenhys@gmail.com>
2012-03-10 19:41:19 +0900

    remove needless whitespaces.

--- examples/slide-show-demo.scm
+++ examples/slide-show-demo.scm
@@ -14,16 +14,16 @@
 	 [dst 0] [annot 0])
     (width! page 800)
     (height! page 600)
-    
+
     (rgb-fill! page r g b)
-    
+
     (rectangle page 0 0 800 600)
     (fill page)
-    
+
     (rgb-fill! page (- 1.0 r) (- 1.0 g) (- 1.0 b))
-    
+
     (font-and-size! page font 30)
-    
+
     (begin-text page)
     (text-matrix! page 0.8 0.0 0.0 1.0 0.0 0.0)
     (text-out page 50 530 caption)
@@ -33,20 +33,20 @@
     (text-out page
 	      55 300 "Type Ctrl+L in order to return from full screen mode")
     (end-text page)
-    
+
     (slide-show! page style 5.0 1.0)
 
     (font-and-size! page font 20)
-    
+
     (if next
-	(begin 
+	(begin
 	  (begin-text page)
 	  (text-out page 680 50 "Next=>")
 	  (end-text page)
 	  ))
 
     (if next
-	(begin 
+	(begin
 	  (begin-text page)
 	  (text-out page 50 50 "<=Prev")
 	  (end-text page)
@@ -66,7 +66,7 @@
 	(vector-set! pages i (add-page pdf))
 	(set! i (+ i 1))))
 
-    (map-with-index 
+    (map-with-index
      (^ (idx entry)
        (let* ([caption (~ entry 1)]
 	      [effect (~ entry 0)])
@@ -80,7 +80,7 @@
 		(print-page (~ pages idx) caption font effect
 			    (~ pages (- idx 1))
 			    #f))
-	       (else 
+	       (else
 		;; prev next
 		(print-page (~ pages idx) caption font effect
 			    (~ pages (- idx 1))
@@ -102,9 +102,9 @@
        (,HPDF_TS_GLITTER_RIGHT "HPDF_TS_WIPE_RIGHT")
        (,HPDF_TS_GLITTER_TOP_LEFT_TO_BOTTOM_RIGHT "HPDF_TS_WIPE_RIGHT")
        (,HPDF_TS_REPLACE "HPDF_TS_WIPE_RIGHT")))
-    
+
     (page-mode! pdf HPDF_PAGE_MODE_FULL_SCREEN)
-    
+
     (save-to-file pdf filename)
-    
+
     (free pdf)))
