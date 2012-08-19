d3a77bc21a7650b35c6549e3ab03c7ecd142f08b
HAYASHI Kentaro <kenhys@gmail.com>
2012-08-20 00:01:36 +0900

    count total error

--- test/run-test.sh
+++ test/run-test.sh
@@ -2,8 +2,13 @@
 
 basedir=`dirname $0`
 
+errnum=0
 for test in $basedir/../test/test*.scm
 do
   echo "Running test $test"
   gosh -I. -I../lib -I./lib -I./src -I../src -I$basedir/../ -I$basedir/../lib $test -vp
+  if [ $? -ne 0 ]; then
+      errnum=`expr $errnum + 1`
+  fi
 done
+exit $errnum
